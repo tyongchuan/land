525cfeda138bbba9afcc5db664a09e0d24ffebbc
ychtgx <ychtgx@gmail.com>
2020-08-31 14:38:02 +0800

    fix: error when ph_am or ph_en is null

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+deno install -f --allow-read --allow-net --allow-env mydict2.ts
--- mydict2.ts
+++ mydict2.ts
@@ -135,7 +135,7 @@ function display(data: any) {
       }
     }
     for (const symbol of symbols) {
-      console.log(` 英[${yellow(symbol["ph_en"])}] 美[${yellow(symbol["ph_am"])}]`);
+      console.log(` 英[${yellow(symbol["ph_en"] || "")}] 美[${yellow(symbol["ph_am"] || "")}]`);
       for (const part of symbol.parts) {
         console.log(` ${green("◆")} ${cyan(part.part + part.means)}`);
       }
